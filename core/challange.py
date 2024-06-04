8b524657caad4760628487c86eaa7c1a51f8d3e7
FlaBBB <f12345ff67@gmail.com>
2024-06-04 11:42:04 +0700

    refactor: most of the code, make it more clean

---
+++ core/challange.py
@@ -0,0 +1,96 @@
+import codecs
+import logging
+import os
+import re
+from os import path
+from urllib.parse import urlparse
+
+import helper
+
+
+class Challenge(object):
+    def __init__(self, name, category="", description="", files=None, value=0):
+        self.name = name
+        self.category = category
+        self.description = description
+        self.logger = logging.getLogger(__name__)
+        self.files = self.collect_files(files, description)
+        self.value = value
+
+    def __eq__(self, value: object) -> bool:
+        return (
+            self.name == value.name
+            and self.category == value.category
+            and self.description == value.description
+            and self.files == value.files
+            and self.value == value.value
+        )
+
+    def to_dict(self):
+        return {
+            "name": self.name,
+            "category": self.category,
+            "description": self.description,
+            "files": self.files,
+            "value": self.value,
+        }
+
+    @staticmethod
+    def from_dict(data):
+        return Challenge(
+            name=data["name"],
+            category=data["category"],
+            description=data["description"],
+            files=data["files"],
+            value=data["value"],
+        )
+
+    @staticmethod
+    def collect_files(files, description=""):
+        files = files or []
+        files.extend(
+            re.findall(
+                r"https?:\/\/\w+(?:\.\w+)+(?:\/[?=&\w._-]+)+", description, re.DOTALL
+            )
+        )
+        return files
+
+    @staticmethod
+    def escape_filename(filename):
+        return re.sub(r"[^\w\s\-.()]", "", filename.strip()).replace(" ", "_")
+
+    def get_challenge_path(self):
+        return path.join(
+            self.escape_filename(self.category), self.escape_filename(self.name)
+        ).replace(" ", "_")
+
+    def download_file(self, url, file_path, override=False):
+        if "google.com" in url:
+            helper.gdown(url, file_path, enable=override)
+            return
+
+        name = self.escape_filename(path.basename(urlparse(url).path))
+
+        file_path = os.path.join(file_path, name)
+        if not os.path.exists(file_path) or override:
+            response = self.ctf.session.get(url, stream=True)
+            helper.download(response, file_path)
+
+    def download_all_files(self, force=False):
+        for file_url in self.files:
+            self.download_file(file_url, self.get_challenge_path(), force)
+
+    def dump(self):
+        # Create challenge directory if not exist
+        challenge_path = self.get_challenge_path()
+        os.makedirs(challenge_path, exist_ok=True)
+
+        with codecs.open(
+            path.join(challenge_path, "ReadMe.md"), "wb", encoding="utf-8"
+        ) as f:
+            f.write(f"Name: {self.name}\n")
+            f.write(f"Value: {self.value}\n")
+            f.write(f"Description: {self.description}\n")
+            self.logger.info(
+                f"Creating Challenge [{self.category or 'No Category'}] {self.name}"
+            )
---
+++ core/ctfs/ctf.py
@@ -0,0 +1,102 @@
+import codecs
+import json
+import logging
+from os import path
+from typing import List
+from urllib.parse import urljoin
+
+from cloudscraper import create_scraper
+from ctfs.ctfd import CTFd
+from ctfs.rctf import rCTF
+from requests.utils import CaseInsensitiveDict
+
+from core.challange import Challenge
+
+
+class NotCompatiblePlatformException(Exception):
+    pass
+
+
+class CTF(object):
+    def __init__(self, url):
+        self.name = self.__class__.__name__
+        self.url = url
+        self.session = create_scraper()
+        self.logger = logging.getLogger(__name__)
+        self.challanges: List[Challenge] = []
+
+    def iter_challenges(self):
+        raise NotImplementedError()
+
+    def login(self, no_login=False, **kwargs):
+        raise NotImplementedError()
+
+    def credential_to_dict(self):
+        raise NotImplementedError()
+
+    def credential_from_dict(self, credential):
+        raise NotImplementedError()
+
+    def logout(self):
+        self.session.get(urljoin(self.url, "/logout"))
+
+    def save_config(self):
+        if self.name == "CTF":
+            raise NotCompatiblePlatformException()
+
+        with codecs.open("config.json", "w", encoding="utf-8") as f:
+            f.write(
+                json.dumps(
+                    {
+                        "platform": self.name,
+                        "url": self.url,
+                        "credentials": self.credential_to_dict(),
+                        "challenges": [
+                            challenge.to_dict() for challenge in self.challanges
+                        ],
+                    },
+                    indent=4,
+                )
+            )
+
+    def load_config(self, config_path="config.json") -> bool:
+        if not path.exists(config_path):
+            return False
+
+        with codecs.open(config_path, "r", encoding="utf-8") as f:
+            config = json.load(f)
+            if config["platform"] != self.name or config["url"] != self.url:
+                raise NotCompatiblePlatformException()
+
+            self.credential_from_dict(config["credentials"])
+
+            for challenge in config["challenges"]:
+                self.challanges.append(Challenge.from_dict(challenge))
+
+        return True
+
+    def save(self):
+        self.challanges = list(self.iter_challenges())
+        for challenge in self.challanges:
+            challenge.dump()
+            challenge.download_all_files()
+
+        self.save_config()
+
+    def update(self, force=False):
+        new_challange = list(self.iter_challenges())
+        is_changed = False
+        for nc, oc in zip(new_challange, self.challanges):
+            if nc != oc:
+                nc.dump()
+                nc.download_all_files(force)
+                is_changed = True
+
+        if is_changed:
+            self.challanges = new_challange
+            self.save_config()
+        else:
+            self.logger.info("No changes found")
+
+
+CTFs = CaseInsensitiveDict(data={"CTFd": CTFd, "rCTF": rCTF})
